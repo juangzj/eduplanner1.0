c198326f5f711be70483254d746677192e9d7e36
Juan Goyes <juanjosegoyeszambrano@gmail.com>
2026-04-26 17:02:27 -0500

    feat: Enhance environment detection to include RENDER_EXTERNAL_HOSTNAME for production settings

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -4,8 +4,9 @@ from .base import *
 
 
 django_env = os.getenv('DJANGO_ENV', 'development').lower()
+is_render = bool(os.getenv('RENDER_EXTERNAL_HOSTNAME') or os.getenv('RENDER'))
 
-if django_env in {'production', 'prod'}:
+if django_env in {'production', 'prod'} or is_render:
     from .prod import *
 else:
     from .dev import *
